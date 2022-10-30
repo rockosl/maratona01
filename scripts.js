f13ae82180ecfc8a0dfcdb5882edbe9ba7b4fd2b
Ricardo <rockobaschirotto@gmail.com>
2022-10-30 12:36:50 -0300

    Implementando javascript

---
+++ scripts.js
@@ -0,0 +1,68 @@
+const Modal = {
+    toggle() {
+        document.querySelector('.modal-overlay').classList.toggle('active')
+    }
+}
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2022', 
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 5000000,
+        date: '23/01/2022', 
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2022', 
+    },
+]
+
+const Transaction = {
+    incomes() {
+
+    },
+    expenses() {
+
+    },
+    total() {
+
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction) {
+
+
+
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="expense">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+        `
+
+        return html
+    }
+}
+
+DOM.addTransaction(transactions[0])
+DOM.addTransaction(transactions[1])
+DOM.addTransaction(transactions[2])
+
+transactions.forEach(function(transaction) {})
