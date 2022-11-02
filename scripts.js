7982dda57c32a79e573e0cc3bbdea0afd4635015
Ricardo Baschirotto <rockobaschirotto@gmail.com>
2022-11-02 10:06:55 -0300

    Formatado números da tabela

--- scripts.js
+++ scripts.js
@@ -8,22 +8,43 @@ const transactions = [
     {
         id: 1,
         description: 'Luz',
-        amount: -50000,
+        amount: -50012,
         date: '23/01/2022', 
     },
     {
         id: 2,
         description: 'Website',
-        amount: 5000000,
+        amount: 500001,
         date: '23/01/2022', 
     },
     {
         id: 3,
         description: 'Internet',
-        amount: -20000,
+        amount: -20090,
+        date: '23/01/2022', 
+    },
+    {
+        id: 4,
+        description: 'App',
+        amount: 20000,
         date: '23/01/2022', 
     },
 ]
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+        value = Number(value) /100
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
 
 const Transaction = {
     incomes() {
@@ -40,18 +61,18 @@ const Transaction = {
 const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'),
     addTransaction(transaction, index) {
-        console.log("teste")
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
         DOM.transactionsContainer.appendChild(tr)
     },
     innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
-
+        const amount = Utils.formatCurrency(transaction.amount)
 
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="expense">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover transação">
@@ -62,8 +83,7 @@ const DOM = {
     }
 }
 
-DOM.addTransaction(transactions[0])
-DOM.addTransaction(transactions[1])
-DOM.addTransaction(transactions[2])
 
-transactions.forEach(function(transaction) {})
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
