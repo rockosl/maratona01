5df8fdb4f870d7831f250cb62a4a3ff30cdc262a
Ricardo Baschirotto <rockobaschirotto@gmail.com>
2022-11-04 23:31:24 -0300

    Implementado métodos dos cálculos

--- scripts.js
+++ scripts.js
@@ -9,62 +9,68 @@ const transactions = [
         id: 1,
         description: 'Luz',
         amount: -50012,
-        date: '23/01/2022', 
+        date: '23/01/2022',
     },
     {
         id: 2,
         description: 'Website',
-        amount: 500001,
-        date: '23/01/2022', 
+        amount: 500000,
+        date: '23/01/2022',
     },
     {
         id: 3,
         description: 'Internet',
         amount: -20090,
-        date: '23/01/2022', 
+        date: '23/01/2022',
     },
     {
         id: 4,
         description: 'App',
-        amount: 20000,
-        date: '23/01/2022', 
+        amount: 200000,
+        date: '23/01/2022',
     },
 ]
 
-const Utils = {
-    formatCurrency(value) {
-        const signal = Number(value) < 0 ? "-" : ""
-
-        value = String(value).replace(/\D/g, "")
-        value = Number(value) /100
-        value = value.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        })
-
-        return signal + value
-    }
-}
-
 const Transaction = {
-    incomes() {
+    all: transactions,
+    add(transaction) {
+        Transaction.all.push(transaction)
 
+        App.reload()
+    },
+
+    incomes() {
+        let income = 0;
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount > 0) {
+                income = income + transaction.amount;
+            }
+        })
+        return income
     },
     expenses() {
-
+        let expense = 0;
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount < 0) {
+                expense = expense + transaction.amount;
+            }
+        })
+        return expense
     },
     total() {
-
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
 const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'),
+
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
         DOM.transactionsContainer.appendChild(tr)
     },
+
     innerHTMLTransaction(transaction) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
@@ -80,10 +86,57 @@ const DOM = {
         `
 
         return html
+    },
+
+    updateBlance() {
+        document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
+    }
+
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+        value = Number(value) / 100
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
     }
 }
 
+const App = {
+    init() {
 
-transactions.forEach(function(transaction) {
-    DOM.addTransaction(transaction)
+        Transaction.all.forEach(function (transaction) {
+            DOM.addTransaction(transaction)
+        })
+
+        DOM.updateBlance()
+    },
+    reload() {
+        App.clearTransactions();
+        App.init();
+    },
+}
+
+App.init()
+
+Transaction.add({
+    id: 33,
+    description: 'Agua',
+    amount: 2000,
+    date: '20/12/2022'
 })
+
+
